1d70b609405204f7bfca62f3b70d31ce7f9b2988
fanyangxi <fanyangxi@live.cn>
2016-06-18 10:47:35 +0800

    init v2

---
+++ src/main/java/bookish/Application.java
@@ -0,0 +1,17 @@
+package bookish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by yxfan on 9/28/15.
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main (String[] args) {
+
+        SpringApplication.run(Application.class, args);
+    }
+}
